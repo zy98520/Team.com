0bbe66ec0aa03453428a0b1a1769b33500569b35
zy98520 <1781134773@qq.com>
2017-09-09 15:03:19 +0800

    原框架

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from "@angular/common/http"
+
+
 
 import { AppComponent } from './app.component';
+import { LooksComponent } from './looks/looks.component';
+import { IndexComponent } from './index/index.component';
+import { LoginComponent } from './login/login.component';
+import { SearchComponent } from './search/search.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { FishionWeekComponent } from './fishion-week/fishion-week.component';
+import { LovesComponent } from './loves/loves.component';
+import { SharesComponent } from './shares/shares.component';
+
+//导入路由
+import {AppRoutingModule} from './app-routing.module';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LooksComponent,
+    IndexComponent,
+    LoginComponent,
+    SearchComponent,
+    PageNotFoundComponent,
+    FishionWeekComponent,
+    LovesComponent,
+    SharesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
